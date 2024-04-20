1b84f7070d71b73a92214e44fc74f6a7b69cbe94
maxmudovdev <maxmudovdev@gmail.com>
2024-04-20 23:05:24 +0500

    first commit

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -3,9 +3,11 @@ import { Helmet } from "react-helmet-async";
 
 export const SEO = ({ title }) => {
   return (
+    
     <Helmet>
       <title className="capitalize">{title}</title>
       <meta name="description" content="Discover the latest trends in fashion for men, women, and kids with our diverse collection of stylish and comfortable clothing." />
     </Helmet>
+
   );
 };
